39d6c8328af221d3d4aae6a527afdec02704709f
Araime <alvonsa09121985@gmail.com>
2021-05-12 17:58:13 +0400

    Create directory using code Download file in directory

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os
 import requests
+
+directory = 'C:/Users/varfolomeyfever/PycharmProjects/Instagram photo/image/'
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 filename = 'hubble.jpeg'
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
@@ -6,5 +12,5 @@ url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with open(directory + filename, 'wb') as file:
     file.write(response.content)
