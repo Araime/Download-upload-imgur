429f36e970eef6ed1ea53e1056a534e77be489b5
Araime <alvonsa09121985@gmail.com>
2021-05-12 21:12:34 +0400

    Add script for download img from space x Test script

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ def get_img(url, filename, directory):
     return print('Ok.')
 
 
+def get_launch_img(url_2, directory):
+    response_2 = requests.get(url_2)
+    response_2.raise_for_status()
+    launch = response_2.json()
+    launch_images = launch['links']['flickr_images']
+    i = 1
+    for image in launch_images:
+        with open(directory + f'spacex{i}.jpg', 'wb') as file:
+            file.write(response_2.content)
+        i += 1
+    return print('Ok.')
+
+
 if __name__ == '__main__':
     directory = 'C:/Users/varfolomeyfever/PycharmProjects/Instagram photo/images/'
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    url_2 = 'https://api.spacexdata.com/v3/launches/13'
     filename = 'hubble.jpeg'
 
     if not os.path.exists(directory):
@@ -24,9 +38,12 @@ if __name__ == '__main__':
     except requests.exceptions.HTTPError as error:
         exit(f'Введена неправильная ссылка:\n{error}')
 
-    url_2 = 'https://api.spacexdata.com/v3/launches/13'
     response_2 = requests.get(url_2)
     response_2.raise_for_status()
     launch = response_2.json()
     launch_images = launch['links']['flickr_images']
-    pprint(launch_images)
+
+    # try:
+    #     get_launch_img(url_2, directory)
+    # except requests.exceptions.HTTPError as error:
+    #     exit(f'Введена неправильная ссылка:\n{error}')
