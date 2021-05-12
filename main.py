9e6b59ff26b86f5b4e9e68a191b00671c890e847
Araime <alvonsa09121985@gmail.com>
2021-05-12 20:45:32 +0400

    Fix directory path Add request for space x

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import requests
+from pprint import pprint
 
 
-def get_pic(url, filename, directory):
+def get_img(url, filename, directory):
     response = requests.get(url)
     response.raise_for_status()
     with open(directory + filename, 'wb') as file:
@@ -11,7 +12,7 @@ def get_pic(url, filename, directory):
 
 
 if __name__ == '__main__':
-    directory = 'C:/Users/varfolomeyfever/PycharmProjects/Instagram photo/image/'
+    directory = 'C:/Users/varfolomeyfever/PycharmProjects/Instagram photo/images/'
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
     filename = 'hubble.jpeg'
 
@@ -19,6 +20,13 @@ if __name__ == '__main__':
         os.makedirs(directory)
 
     try:
-        get_pic(url, filename, directory)
+        get_img(url, filename, directory)
     except requests.exceptions.HTTPError as error:
         exit(f'Введена неправильная ссылка:\n{error}')
+
+    url_2 = 'https://api.spacexdata.com/v3/launches/13'
+    response_2 = requests.get(url_2)
+    response_2.raise_for_status()
+    launch = response_2.json()
+    launch_images = launch['links']['flickr_images']
+    pprint(launch_images)
