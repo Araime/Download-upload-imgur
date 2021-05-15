f356bcf08e1a7e37d53e5bd40923ee56cc9fdc5c
Araime <alvonsa09121985@gmail.com>
2021-05-15 19:02:51 +0400

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def fetch_spacex_last_launch(url_2, directory):
-    response_2 = requests.get(url_2)
+def fetch_spacex_last_launch(spacex_url, directory):
+    response_2 = requests.get(spacex_url)
     response_2.raise_for_status()
     launch = response_2.json()
     launch_images = launch['links']['flickr_images']
