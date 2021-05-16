69b204739635e4131dbbf58aa038c57274c7979b
Araime <alvonsa09121985@gmail.com>
2021-05-16 22:04:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ from PIL import Image
 
 
 if __name__ == '__main__':
-    directory = os.getcwd() + '/images/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    images_list = sorted(os.listdir('Images/'))
 
-    # images_list = sorted(os.listdir('Images/'))
-
-    # for img in images_list:
-    #     if img.endswith(('.jpg', '.png')):
-    #         split_image_name = os.path.splitext(images_list[img])
-    #         image_name = split_image_name[0]
-    #         image = Image.open(f'images/{img}')
-    #         image.thumbnail((1080, 1080))
-    #         image.save(f'images/new {image_name}.jpg', format='JPEG')
+    for img in images_list:
+        if img.endswith(('.jpg', '.png')):
+            split_image_name = os.path.splitext(images_list[img])
+            image_name = split_image_name[0]
+            image = Image.open(f'images/{img}')
+            image.thumbnail((1080, 1080))
+            image.save(f'images/new {image_name}.jpg', format='JPEG')
