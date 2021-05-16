afd568082e4ceb85de81eaeb81ed5dbb5b81c9e0
Araime <alvonsa09121985@gmail.com>
2021-05-16 22:59:44 +0400

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -3,16 +3,16 @@ import requests
 
 
 def fetch_spacex_last_launch(spacex_url, directory):
-    response_2 = requests.get(spacex_url)
-    response_2.raise_for_status()
-    launch = response_2.json()
+    response = requests.get(spacex_url)
+    response.raise_for_status()
+    launch = response.json()
     launch_images = launch['links']['flickr_images']
     for image_number, image in enumerate(launch_images):
         image_url = image
-        response_3 = requests.get(image_url)
-        response_3.raise_for_status()
+        response = requests.get(image_url)
+        response.raise_for_status()
         with open(directory + f'spacex{image_number + 1}.jpg', 'wb') as file:
-            file.write(response_3.content)
+            file.write(response.content)
             print(f'Image spacex{image_number + 1}.jpg downloaded')
     return print('Process completed')
 
