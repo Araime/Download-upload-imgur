f04681de016c26391ea31b331fc9b7d8c74c2d39
Araime <alvonsa09121985@gmail.com>
2021-05-16 11:50:47 +0400

    Add a script for opening, compressing, and resaving images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from PIL import image
+from PIL import Image
 from fetch_hubble import fetch_hubble
 from fetch_spacex import fetch_spacex_last_launch
 from urllib.parse import urlsplit, unquote_plus
@@ -23,23 +23,31 @@ if __name__ == '__main__':
     hubble_url = 'http://hubblesite.org/api/v3/images/stsci_gallery'
     spacex_url = 'https://api.spacexdata.com/v3/launches/13'
 
-    response = requests.get(hubble_url, verify=False)
-    response.raise_for_status()
-    image_collection_link = response.json()
-    for image in image_collection_link:
-        image_id = image['id']
-        url = f'http://hubblesite.org/api/v3/image/{image_id}'
-        response = requests.get(url)
-        response.raise_for_status()
-        hubble_response = response.json()
-        image_link = hubble_response['image_files'][-1]['file_url']
-        extension = get_file_extension(image_link)
-        try:
-            fetch_hubble(image_link, directory, image_id, extension)
-        except requests.exceptions.HTTPError as error:
-            exit(f'Введена неправильная ссылка:\n{error}')
+    # response = requests.get(hubble_url, verify=False)
+    # response.raise_for_status()
+    # image_collection_link = response.json()
+    # for image in image_collection_link:
+    #     image_id = image['id']
+    #     url = f'http://hubblesite.org/api/v3/image/{image_id}'
+    #     response = requests.get(url)
+    #     response.raise_for_status()
+    #     hubble_response = response.json()
+    #     image_link = hubble_response['image_files'][-1]['file_url']
+    #     extension = get_file_extension(image_link)
+    #     try:
+    #         fetch_hubble(image_link, directory, image_id, extension)
+    #     except requests.exceptions.HTTPError as error:
+    #         exit(f'Введена неправильная ссылка:\n{error}')
 
     # try:
     #     fetch_spacex_last_launch(spacex_url, directory)
     # except requests.exceptions.HTTPError as error:
     #     exit(f'Введена неправильная ссылка:\n{error}')
+
+    images_list = sorted(os.listdir('Images/'))
+
+    for img in images_list:
+        if img.endswith(('.jpg', '.png')):
+            image = Image.open(f'images/{img}')
+            image.thumbnail((1080, 1080))
+            image.save(f'images/new {img}', format='JPEG')
