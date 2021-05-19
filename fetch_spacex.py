6c3beda347e0a6fdd4e8bbbcc35ab168a35930c4
Araime <alvonsa09121985@gmail.com>
2021-05-19 23:41:05 +0400

    Add argparse

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import requests
 
 
@@ -18,8 +19,15 @@ def fetch_spacex_last_launch(spacex_url, directory):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Программа принимает ссылку на фотографии запуска'
+                                                 ' в качестве аргумента')
+    parser.add_argument('link', help='Ссылка на фотографии запуска Spacex, например'
+                                     ' https://api.spacexdata.com/v3/launches/13')
+    args = parser.parse_args()
+    spacex_url = args.link
+
     directory = os.getcwd() + '/images/'
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    fetch_spacex_last_launch('https://api.spacexdata.com/v3/launches/13', directory)
+    fetch_spacex_last_launch(spacex_url, directory)
