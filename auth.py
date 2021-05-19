cbc041df54156d13eaf0fc2b5736f29fc25c4249
Araime <alvonsa09121985@gmail.com>
2021-05-19 12:46:29 +0400

    Add authorization on Imgur.com

---
+++ auth.py
@@ -0,0 +1,36 @@
+"""
+Вот как вы аутентифицируете себя! Важно
+отметить, что этот сценарий будет использоваться в других примерах, поэтому
+настройте тестового пользователя с учетными данными API и настройте их в auth.ini.
+"""
+import os
+from dotenv import load_dotenv
+from imgurpython import ImgurClient
+
+load_dotenv()
+
+
+def authenticate():
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+
+    client = ImgurClient(client_id, client_secret)
+
+    authorization_url = client.get_auth_url('pin')
+
+    print("Go to the following URL: {0}".format(authorization_url))
+
+    pin = input("Enter pin code: ")
+
+    credentials = client.authorize(pin, 'pin')
+    client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
+
+    print("Authentication successful! Here are the details:")
+    print("   Access token:  {0}".format(credentials['access_token']))
+    print("   Refresh token: {0}".format(credentials['refresh_token']))
+
+    return client
+
+
+if __name__ == "__main__":
+    authenticate()
