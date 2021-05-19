3082753f43358d6dc24fc6f10168bafcd6cf647e
Araime <alvonsa09121985@gmail.com>
2021-05-20 00:12:27 +0400

    Add removing unused files

--- image processing.py
+++ image processing.py
@@ -3,11 +3,13 @@ from PIL import Image
 
 
 if __name__ == '__main__':
-    images_list = sorted(os.listdir('images/'))
+    images_list = sorted(os.listdir('images'))
     for img in images_list:
         if img.endswith(('.jpg', '.png')):
             split_image_name = os.path.splitext(img)
             image_name = split_image_name[0]
-            image = Image.open(f'images/{img}')
+            image = Image.open(f'images\\{img}')
             image.thumbnail((1080, 1080))
-            image.save(f'images/{image_name}.jpg', format='JPEG')
+            image.save(f'images\\{image_name}.jpg', format='JPEG')
+            if img.endswith('.png'):
+                os.remove(f'images\\{img}')
