c678418b4c8b0e613eb0926724637a189c92b35b
Araime <alvonsa09121985@gmail.com>
2021-05-19 16:04:52 +0400

    Update upload.py

--- upload.py
+++ upload.py
@@ -19,32 +19,20 @@ if __name__ == "__main__":
 	client = authenticate()
 	album = 'Space-themed photos'
 
-	#  вариант 1
+	directory = os.getcwd() + '/images/'
+	os.chdir(directory)
 
-	image_path = 'images/3861.jpg'
-	config = {
-		'album': album,
-		'name': 'name',
-		'description': 'Photo from internet {0}'.format(datetime.now())
-	}
-	image = upload_image(client, image_path, config)
+	for image_name in listdir('images'):
+		if image_name.endswith(('.jpg', '.png')):
+			name_for_split = os.path.splitext(image_name)
+			split_image_name = name_for_split[0]
+			image_path = image_name
+			config = {
+				'album': album,
+				'name': split_image_name,
+				'description': 'Photo from internet {0}'.format(datetime.now())
+			}
+			image = upload_image(client, image_path, config)
 
-	print("Image was posted!")
-	print("You can find it here: {0}".format(image['link']))
-
-	#  вариант 2
-
-	# for image_name in listdir('.'):
-	# 	if isfile:
-	# 		name_for_split = os.path.splitext(image_name)
-	# 		split_image_name = name_for_split[0]
-	# 		image_path = image_name
-	# 		config = {
-	# 			'album': album,
-	# 			'name': split_image_name,
-	# 			'description': 'Photo from internet {0}'.format(datetime.now())
-	# 		}
-	# 		image = upload_image(client, image_path, config)
-	#
-	# 		print("Image was posted!")
-	# 		print("You can find it here: {0}".format(image['link']))
+			print("Image was posted!")
+			print("You can find it here: {0}".format(image['link']))
