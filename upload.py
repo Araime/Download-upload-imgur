1e0453154cee90084d4c3c608c7271fa60a9af59
Araime <alvonsa09121985@gmail.com>
2021-05-19 21:13:54 +0400

    Merge auth.py and upload.py together

--- upload.py
+++ upload.py
@@ -1,9 +1,30 @@
-from auth import authenticate
 from datetime import datetime
 import os
+from dotenv import load_dotenv
+from imgurpython import ImgurClient
 from os import listdir
 from os.path import isfile
 from os.path import join as joinpath
+
+load_dotenv()
+
+def authenticate():
+	client_id = os.getenv('CLIENT_ID')
+	client_secret = os.getenv('CLIENT_SECRET')
+	client = ImgurClient(client_id, client_secret)
+
+	authorization_url = client.get_auth_url('pin')
+	print("Go to the following URL: {0}".format(authorization_url))
+
+	pin = input("Enter pin code: ")
+	credentials = client.authorize(pin, 'pin')
+	client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
+
+	print("Authentication successful! Here are the details:")
+	print("   Access token:  {0}".format(credentials['access_token']))
+	print("   Refresh token: {0}".format(credentials['refresh_token']))
+
+	return client
 
 
 def upload_image(client, image_path, config):
@@ -16,19 +37,20 @@ def upload_image(client, image_path, config):
 
 
 if __name__ == "__main__":
-	client = authenticate()
+	# client = authenticate()
 	album = 'Space-themed photos'
 	image_path = 'images\\3861.jpg'
+	print(image_path)
 
-	config = {
-		'album': album,
-		'name': 'name',
-		'description': 'Photo from internet {0}'.format(datetime.now())
-	}
-	image = upload_image(client, image_path, config)
-
-	print("Image was posted!")
-	print("You can find it here: {0}".format(image['link']))
+	# config = {
+	# 	'album': album,
+	# 	'name': 'name',
+	# 	'description': 'Photo from internet {0}'.format(datetime.now())
+	# }
+	# image = upload_image(client, image_path, config)
+	#
+	# print("Image was posted!")
+	# print("You can find it here: {0}".format(image['link']))
 
 	# for image_name in listdir('.'):
 	# 	if image_name.endswith(('.jpg', '.png')):
