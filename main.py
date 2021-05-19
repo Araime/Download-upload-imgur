6b33fb98f0dba28ab203039e3165f2c928d1b1cf
Araime <alvonsa09121985@gmail.com>
2021-05-19 10:45:34 +0400

    Fix working with images: rename , resize, save

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from PIL import Image
 
 if __name__ == '__main__':
     images_list = sorted(os.listdir('Images/'))
-
     for img in images_list:
         if img.endswith(('.jpg', '.png')):
-            split_image_name = os.path.splitext(images_list[img])
+            split_image_name = os.path.splitext(img)
             image_name = split_image_name[0]
             image = Image.open(f'images/{img}')
             image.thumbnail((1080, 1080))
-            image.save(f'images/new {image_name}.jpg', format='JPEG')
+            image.save(f'images/{image_name}.jpg', format='JPEG')
