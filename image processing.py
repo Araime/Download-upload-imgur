15c5249bf50bd4aa43ede6458af9157214b9577a
Araime <alvonsa09121985@gmail.com>
2021-05-23 22:14:26 +0400

    Make the script compatible with *nix and Windows

--- image processing.py
+++ image processing.py
@@ -3,13 +3,17 @@ from PIL import Image
 
 
 if __name__ == '__main__':
-    images_list = sorted(os.listdir('images'))
+    dirname = 'images'
+
+    images_list = sorted(os.listdir(dirname))
     for img in images_list:
         if img.endswith(('.jpg', '.png')):
             split_filename = os.path.splitext(img)
-            image_name = split_filename[0]
-            image = Image.open(f'images\\{img}')
+            image_name = split_filename[0] + '.jpg'
+            old_image_path = os.path.join(dirname, img)
+            new_image_path = os.path.join(dirname, image_name)
+            image = Image.open(old_image_path)
             image.thumbnail((1080, 1080))
-            image.save(f'images\\{image_name}.jpg', format='JPEG')
+            image.save(new_image_path, format='JPEG')
             if img.endswith('.png'):
-                os.remove(f'images\\{img}')
+                os.remove(old_image_path)
