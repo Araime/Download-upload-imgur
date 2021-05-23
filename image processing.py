43049e238c0a6c5934734271707b9cbbd98627b2
Araime <alvonsa09121985@gmail.com>
2021-05-23 23:14:48 +0400

    Update image processing.py

--- image processing.py
+++ image processing.py
@@ -9,11 +9,13 @@ if __name__ == '__main__':
     for img in images_list:
         if img.endswith(('.jpg', '.png')):
             split_filename = os.path.splitext(img)
-            image_name = split_filename[0] + '.jpg'
+            image_name = f'{split_filename[0]}.jpg'
             old_image_path = os.path.join(dirname, img)
             new_image_path = os.path.join(dirname, image_name)
             image = Image.open(old_image_path)
             image.thumbnail((1080, 1080))
             image.save(new_image_path, format='JPEG')
-            if img.endswith('.png'):
-                os.remove(old_image_path)
+    for img in images_list:
+        if img.endswith('.png'):
+            image_path = os.path.join(dirname, img)
+            os.remove(image_path)
