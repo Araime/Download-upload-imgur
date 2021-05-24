a93e3110fa0ee64f4626b3ea276da6aa5caf4d0b
Araime <alvonsa09121985@gmail.com>
2021-05-24 23:42:51 +0400

    Expand if, make the code flatter

--- image_processing.py
+++ image_processing.py
@@ -7,12 +7,10 @@ if __name__ == '__main__':
 
     images_list = sorted(os.listdir(dirname))
     for img in images_list:
-        if img.endswith(('.jpg', '.png')):
-            split_filename = os.path.splitext(img)
-            filename, extension = split_filename
-            image = Image.open(os.path.join(dirname, img))
-            image.thumbnail((1080, 1080))
-            image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
-    for img in images_list:
+        split_filename = os.path.splitext(img)
+        filename, extension = split_filename
+        image = Image.open(os.path.join(dirname, img))
+        image.thumbnail((1080, 1080))
+        image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
         if img.endswith('.png'):
             os.remove(os.path.join(dirname, img))
