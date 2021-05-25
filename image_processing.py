68b6fdcdd22718c1ff2c6d2598be1c9cd1933974
Araime <alvonsa09121985@gmail.com>
2021-05-26 00:44:02 +0400

    Add not and continue

--- image_processing.py
+++ image_processing.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
 
     images_list = sorted(os.listdir(dirname))
     for img in images_list:
+        if not img.endswith(('.jpg', '.png')):
+            continue
         split_filename = os.path.splitext(img)
         filename, extension = split_filename
         image = Image.open(os.path.join(dirname, img))
         image.thumbnail((1080, 1080))
         image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
-        if img.endswith('.png'):
+        if not img.endswith('.jpg'):
             os.remove(os.path.join(dirname, img))
