b749199e84c2366574a3a5b601a676e2ec8299c4
Araime <alvonsa09121985@gmail.com>
2021-05-27 17:58:47 +0400

    Revert "Add variable name"
    This reverts commit 86b80504

--- image_processing.py
+++ image_processing.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         split_filename = os.path.splitext(img)
         filename, extension = split_filename
         image = Image.open(os.path.join(dirname, img))
-        compressed_image = image.thumbnail((1080, 1080))
-        compressed_image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
+        image.thumbnail((1080, 1080))
+        image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
         if not img.endswith('.jpg'):
             os.remove(os.path.join(dirname, img))
