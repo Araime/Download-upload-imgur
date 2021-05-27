5556d70adfd189af601ab8d614c35cfbf525ab9d
Araime <alvonsa09121985@gmail.com>
2021-05-27 18:01:56 +0400

    Link the numbers to the code

--- image_processing.py
+++ image_processing.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 if __name__ == '__main__':
     dirname = 'images'
+    image_width = 1080
+    image_height = 1080
 
     images_list = sorted(os.listdir(dirname))
     for img in images_list:
@@ -12,7 +14,7 @@ if __name__ == '__main__':
         split_filename = os.path.splitext(img)
         filename, extension = split_filename
         image = Image.open(os.path.join(dirname, img))
-        image.thumbnail((1080, 1080))
+        image.thumbnail((image_width, image_height))
         image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
         if not img.endswith('.jpg'):
             os.remove(os.path.join(dirname, img))
