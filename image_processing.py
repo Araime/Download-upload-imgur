b9f23c21ded778e7320069d90b43f7282b9621e9
Araime <alvonsa09121985@gmail.com>
2021-05-27 18:46:29 +0400

    Break the function into parts

--- image_processing.py
+++ image_processing.py
@@ -2,12 +2,7 @@ import os
 from PIL import Image
 
 
-if __name__ == '__main__':
-    dirname = 'images'
-    image_width = 1080
-    image_height = 1080
-
-    images_list = sorted(os.listdir(dirname))
+def get_processing_image(dirname, image_width, image_height):
     for img in images_list:
         if not img.endswith(('.jpg', '.png')):
             continue
@@ -16,5 +11,19 @@ if __name__ == '__main__':
         image = Image.open(os.path.join(dirname, img))
         image.thumbnail((image_width, image_height))
         image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
+
+
+def deleting_unnecessary_images(dirname, images_list):
+    for img in images_list:
         if not img.endswith('.jpg'):
             os.remove(os.path.join(dirname, img))
+
+
+if __name__ == '__main__':
+    dirname = 'images'
+    images_list = sorted(os.listdir(dirname))
+    image_width = 1080
+    image_height = 1080
+
+    get_processing_image(dirname, image_width, image_height)
+    deleting_unnecessary_images(dirname, images_list)
