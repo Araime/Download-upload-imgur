9e2a495b37aebe3bd4b152f7612f0ff5df4bf55d
Araime <alvonsa09121985@gmail.com>
2021-05-27 18:50:22 +0400

    Change variable name

--- image_processing.py
+++ image_processing.py
@@ -2,8 +2,8 @@ import os
 from PIL import Image
 
 
-def get_processing_image(dirname, image_width, image_height):
-    for img in images_list:
+def get_processing_image(dirname, images, image_width, image_height):
+    for img in images:
         if not img.endswith(('.jpg', '.png')):
             continue
         split_filename = os.path.splitext(img)
@@ -13,17 +13,17 @@ def get_processing_image(dirname, image_width, image_height):
         image.save(f'{os.path.join(dirname, filename)}.jpg', format='JPEG')
 
 
-def deleting_unnecessary_images(dirname, images_list):
-    for img in images_list:
+def deleting_unnecessary_images(dirname, images):
+    for img in images:
         if not img.endswith('.jpg'):
             os.remove(os.path.join(dirname, img))
 
 
 if __name__ == '__main__':
     dirname = 'images'
-    images_list = sorted(os.listdir(dirname))
+    images = sorted(os.listdir(dirname))
     image_width = 1080
     image_height = 1080
 
-    get_processing_image(dirname, image_width, image_height)
-    deleting_unnecessary_images(dirname, images_list)
+    get_processing_image(dirname, images, image_width, image_height)
+    deleting_unnecessary_images(dirname, images)
